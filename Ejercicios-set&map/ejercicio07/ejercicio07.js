a4928e90f9dafa6ab5bb4f011ea1177201095fd7
LozitaL <lozanoruizj@gmail.com>
2024-10-21 11:29:00 +0200

    modified:   Ejercicios-set&map/ejercicio07/ejercicio07.js

--- Ejercicios-set&map/ejercicio07/ejercicio07.js
+++ Ejercicios-set&map/ejercicio07/ejercicio07.js
@@ -4,17 +4,27 @@ function addproducto(){
     let value = 0;
     let producto = "";
     let cantidad = 0;
-    for(i = 0; i < n ; i++){
+    for(i = 0; i < nproductos ; i++){
         value = parseInt(prompt("Codigo"));
         producto = prompt("Nombre del producto");
         cantidad = parseInt(prompt("¿Cuantas existencias tienes?"));
         
-        (map1.has(value))? i-1:map1.set(value,[producto,cantidad]);
-    }
+        (map1.has(value))? i-1 (console.log("Este codigo ya tiene un producto asociado")) : map1.set(value,[producto,cantidad]);
+        if(!map1.has(value)){(map1.has(producto))? i-1 (console.log("Este producto ya ha sido añadido")) : map1.set(value,[producto,cantidad]);}}
+    return map1;
 }
-function modproducto(){
+function modproducto(map1){
+let value = parseInt(prompt("Codigo del producto"));
+let proc = prompt("Nombre del producto:");
+let cant = parseInt(prompt("cantidad:"));
+return map1.set(value,[proc,cant]);
+}
+function obtproducto(map1){
+let value = parseInt(prompt("codigo producto"));
+return map1.get(value);
+}
 
-}
-function obtproducto(){
-
-}
+maptienda = new map();
+maptienda = addproducto();
+obtproducto(maptienda);
+modproducto(maptienda);
