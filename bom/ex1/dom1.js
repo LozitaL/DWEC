7e5a63c1a52423813f171668d0b989f085a2c688
lozita14 <lozanoruizj@gmail.com>
2025-01-12 20:00:58 +0100

    modified:   bom/ex1/a.html 	modified:   bom/ex1/dom1.js 	new file:   bom/ex2/a.html 	new file:   bom/ex2/dom2.js

--- bom/ex1/dom1.js
+++ bom/ex1/dom1.js
@@ -12,3 +12,13 @@ function temporizador(){
 function ventana(){
     document.getElementById("tamañov").innerHTML = "Tamaño: " +window.screen.height + "px";
 }
+
+function abrirv(){
+let nventana = window.open("","","width=800,height=600");
+hacergrande(nventana);
+}
+
+function hacergrande(nvent){
+    nvent.resizeTo(1920,1080);
+    alert("ahora el tamaño es" + window.screen.height+ " X " + window.screen.width);
+}
